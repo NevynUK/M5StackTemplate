2c6368e1c4bb9f1b3e4759cad31baf09c3e3b5c6
Mark Stevens <mark@thepcsite.co.uk>
2025-06-26 11:14:40 +0100

    Remove Mooncake app code and replace with generic app code.

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -7,26 +7,158 @@
 #include <app.h>
 #include <hal/hal.h>
 #include <memory>
+#include <cstring>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+namespace Utils
+{
+    /**
+     * @brief Format a number with commas as thousands separators.
+     * 
+     * @param number The number to format.
+     * @return std::string The formatted number.
+     */
+    std::string NumberWithCommas(uint32_t number);
+
+    /**
+     * @brief Format a number with commas as thousands separators.
+     * 
+     * @param number The number to format.
+     * @return std::string The formatted number.
+     */
+    std::string NumberWithCommas(uint64_t number);
+}
+
+
+/**
+ * @brief Format a number with commas as thousands separators.
+ * 
+ * @param number The number to format.
+ * @return std::string The formatted number.
+ */
+std::string Utils::NumberWithCommas(uint64_t number)
+{
+    uint64_t bufferSize = 27;              // 20 digits, plus 6 commas (max) + terminator.
+    char buffer[27];
+
+    size_t len = snprintf(buffer, bufferSize, "%" PRIu64, number);
+
+    if (len <= 3)
+    {
+        return std::string(buffer);
+    }
+
+    int commas = (len - 1) / 3;
+    char temp[27];
+    strcpy(temp, buffer);
+
+    int tempIndex = len;
+    int bufIndex = len + commas;
+    buffer[bufIndex--] = '\0';
+
+    int count = 0;
+    while (tempIndex > 0)
+    {
+        buffer[bufIndex--] = temp[--tempIndex];
+        count++;
+        if (count == 3 && tempIndex > 0)
+        {
+            buffer[bufIndex--] = ',';
+            count = 0;
+        }
+    }
+
+    return std::string(buffer);
+}
+
+/**
+ * @brief Format a number with commas as thousands separators.
+ * 
+ * @param number The number to format.
+ * @return std::string The formatted number.
+ */
+std::string Utils::NumberWithCommas(uint32_t number)
+{
+    return NumberWithCommas(static_cast<uint64_t>(number));
+}
+
+
+
 extern "C" void app_main(void)
 {
+    std::unique_ptr<HalEsp32> hal = std::make_unique<HalEsp32>();
+    hal->init();
+
     // Application layer initialization callback
-    app::InitCallback_t callback;
+    // app::InitCallback_t callback;
 
-    callback.onHalInjection = []()
-    {
-        // Inject hardware abstraction for ESP32 platform
-        hal::Inject(std::make_unique<HalEsp32>());
-    };
+    // callback.onHalInjection = []()
+    // {
+    //     // Inject hardware abstraction for ESP32 platform
+    //     hal::Inject(std::make_unique<HalEsp32>());
+    // };
 
-    // Application layer startup
-    app::Init(callback);
-    while (!app::IsDone())
+    // // Application layer startup
+    // app::Init(callback);
+    // while (!app::IsDone())
+    // {
+    //     app::Update();
+    //     vTaskDelay(1);
+    // }
+    // app::Destroy();
+
+
+        esp_chip_info_t chip_info;
+    uint32_t flash_size;
+
+    esp_chip_info(&chip_info);
+    printf(
+        "This is %s chip with %d CPU core(s), %s%s%s%s, ", CONFIG_IDF_TARGET, chip_info.cores, (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "", (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "", (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+        (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("Silicon revision v%d.%d, ", major_rev, minor_rev);
+    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
     {
-        app::Update();
-        vTaskDelay(1);
+        printf("Get flash size failed");
+        return;
     }
-    app::Destroy();
+
+    printf("%s bytes %s flash\n", Utils::NumberWithCommas(flash_size).c_str(), (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %s bytes\n", Utils::NumberWithCommas(esp_get_minimum_free_heap_size()).c_str());
+
+    // SDCard *sd_card = SDCard::GetInstance();
+    // sd_card->Setup();
+    // ListFiles(sd_card->GetMountPoint());
+
+    // Display *display = Display::GetInstance();
+    // display->Setup();
+    // uint32_t brightness = 0;
+    // int direction = 10;
+    // while (true)
+    // {
+    //     if (brightness >= 100)
+    //     {
+    //         direction = -10;
+    //     }
+    //     else 
+    //     {
+    //         if (brightness == 0)
+    //         {
+    //             direction = 10;
+    //         }
+    //     }
+    //     brightness += direction;
+    //     display->SetBrightness(brightness);
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // }
+
+    vTaskDelay(portMAX_DELAY / portTICK_PERIOD_MS);
 }
